a177b39d34c559a9b6907629cbfcccfaf7e3be5b
mahbahig <mahbahig2@gmail.com>
2025-01-04 23:27:09 +0200

    Q 3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -17,14 +17,29 @@
             #endregion
 
             #region Q2
+            //Console.Write("Enter a number: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"The number is {number}");
+
+            //for (int i = 1; i <= 12; i++)
+            //{
+            //    Console.Write($" {number*i} ");
+            //}
+            #endregion
+
+            #region Q3
             Console.Write("Enter a number: ");
             int number = int.Parse(Console.ReadLine());
 
             Console.WriteLine($"The number is {number}");
 
-            for (int i = 1; i <= 12; i++)
+            for (int i = 1; i <= number; i++)
             {
-                Console.Write($" {number*i} ");
+                if (i % 2 == 0)
+                {
+                Console.Write($" {i} ");
+                }
             }
             #endregion
         }
