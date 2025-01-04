23dea2c5d185aaa05771f9af33d1fc7f317c0db1
mahbahig <mahbahig2@gmail.com>
2025-01-04 23:24:08 +0200

    Q 2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,14 +5,26 @@
         static void Main(string[] args)
         {
             #region Q1
+            //Console.Write("Enter a number: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"The number is {number}");
+
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    Console.Write($" {i} ");
+            //}
+            #endregion
+
+            #region Q2
             Console.Write("Enter a number: ");
             int number = int.Parse(Console.ReadLine());
 
             Console.WriteLine($"The number is {number}");
 
-            for (int i = 1; i <= number; i++)
+            for (int i = 1; i <= 12; i++)
             {
-                Console.Write($" {i} ");
+                Console.Write($" {number*i} ");
             }
             #endregion
         }
