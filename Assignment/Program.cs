325d41fe239307b6271f301b1f12a4ac4d6edf29
mahbahig <mahbahig2@gmail.com>
2025-01-04 23:14:35 +0200

    Q 1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The number is {number}");
+
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write($" {i} ");
+            }
+            #endregion
         }
     }
 }
